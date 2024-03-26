31a2edf7d5c2899cfa33d2a5de0c75080d6f3bdf
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-03-26 23:34:57 +1300

    Express App Setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)   
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        mssg: 'Welcome to the app'
+    })
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT)
+})
