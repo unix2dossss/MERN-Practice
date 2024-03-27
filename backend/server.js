c0a6910fb56ebd74ab0f087160fc701520c00f19
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-03-28 01:30:10 +1300

    Connect to MongoDB.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -8,6 +9,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)   
     next()
@@ -15,7 +17,10 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts/', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('Connected to database and listening on port!', process.env.PORT)
+    })
+}).catch((error) => {console.log(error)})
